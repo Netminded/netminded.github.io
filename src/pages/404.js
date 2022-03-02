df7e5b79af70d78a8e0c3f8b57351647ee6bd6ef
15collinst <15collinst@clevedonschool.org.uk>
2022-03-02 15:40:35 +0000

    swapped class for className

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout'
 const NotFoundPage = () => {
     return (
         <Layout pTitle="Privacy Policy" isArticle={false} simpleNav={true}>
-            <div class="page-not-found-div">
-              <h2 class="page-not-found-text">😞 Sorry the page you’re looking for does not exist.</h2>
+            <div className="page-not-found-div">
+              <h2 className="page-not-found-text">😞 Sorry the page you’re looking for does not exist.</h2>
             </div>
         </Layout>
     )
