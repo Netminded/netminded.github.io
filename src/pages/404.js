f63dea9ddc87da8e86a5ca42cb61cee70cfcd461
15collinst <15collinst@clevedonschool.org.uk>
2022-03-02 15:27:24 +0000

    added 100% width/height and a gradient background

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout'
 const NotFoundPage = () => {
     return (
         <Layout pTitle="Privacy Policy" isArticle={false} simpleNav={true}>
-            <div>
-              <h2>Sorry the page you’re looking for does not exist.</h2>
+            <div class="page-not-found-div">
+              <h2 class="page-not-found-text">😞 Sorry the page you’re looking for does not exist.</h2>
             </div>
         </Layout>
     )
