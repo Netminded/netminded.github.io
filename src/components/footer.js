89775f70b888539c07f37f3d40e5251cef798abd
15collinst <15collinst@clevedonschool.org.uk>
2022-03-03 17:54:31 +0000

    Redid the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import GooglePlayLogo from '../images/Google_Play_Store_badge.svg'
+import AppleStoreLogo from '../images/apple-store-badge.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faFacebookSquare,
-  faTwitterSquare,
   faLinkedin,
+  faYoutube,
+  faTwitter,
+  faFacebook,
 } from '@fortawesome/free-brands-svg-icons'
 
 
@@ -13,29 +15,73 @@ function Footer() {
         <footer className="footer-container">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-lg-5">
-                        <p className="footer-text">Copyright &copy; <Link className="footer-link" to="/">NetMinded</Link>, a trading name of SeeThru Networks.</p>
-                    </div>
-                    <div className="col-lg-2 text-center footer-social">
+                    <div className='col-1'> 
+                            </div>
+
+                    <div className='col-md-4'>
+                        <h5>Download NetMinded</h5>
+                        <ul>
+                            <a href="#"><img src={AppleStoreLogo} alt="Download on the app store"/></a>
+                            <a href="#"><img src={GooglePlayLogo} alt="Get it on google play" className='google-play-button'/></a>
+                        </ul>
+                        <ul>
                         <a href="https://facebook.com/SeeThruNetworks" target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faFacebookSquare} />
+                            <FontAwesomeIcon icon={faFacebook} />
                         </a> 
                         <a href="https://twitter.com/seethrunetworks" target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faTwitterSquare} />
+                            <FontAwesomeIcon icon={faTwitter} />
                         </a>
                         <a href="https://linkedin.com/company/seethrunetworks" target="_blank" rel="noreferrer">
                             <FontAwesomeIcon icon={faLinkedin} />
                         </a>
-                    </div>
-                    <div className="col-lg-5">
-                        <ul>
-                            <li><Link to="/privacy" className="footer-link" >Privacy</Link></li>
-                            <li><Link to="/terms" className="footer-link" >Terms & Conditions</Link></li>
-                            <li><Link to="/web-terms" className="footer-link" >Web Terms</Link></li>
-                            <li><Link to="/licences" className="footer-link" >Licences</Link></li>
+                        <a href="https://www.youtube.com/channel/UCMye-tTUwQfWAEJ2FQR1ofQ/featured" target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faYoutube} />
+                        </a>
                         </ul>
                     </div>
+
+                    <div className='col-md-2'>
+                        <h5>Support</h5>
+                        <ul>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">FAQs</a></li>
+                            <li><a href="#">Guides</a></li>
+                            <li><a href="#">Open Ticket</a></li>
+                            <li><a href="#">Status</a></li>
+                        </ul>
+                    </div>
+
+                    <div className='col-md-2'>
+                        <h5>Company</h5>
+                        <ul>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Investors</a></li>
+                            <li><a href="#">Press</a></li>
+                            <li><a href="#">Brand Guidelines</a></li>
+                        </ul>
+                    </div>
+
+                    <div className='col-md-2'>
+                        <h5>Resources</h5>
+                        <ul>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Terms</a></li>
+                            <li><a href="#">Web Terms</a></li>
+                            <li><a href="#">Web Licences</a></li>
+                            <li><a href="#">Sitemap</a></li>
+                        </ul>
+                    </div>
+
+                    <div className='col-md-1'>
+                        
+                    </div>
                 </div>
+                {/* <div className="row">
+                    <div className='col-12'>
+                        <p>Copyright©2022•SeeThru Networks•All Rights Reserved</p>
+                    </div>
+                </div> */}
             </div>
         </footer>
     )
