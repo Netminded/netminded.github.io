e50513f7b166fd3a43a7796131f237261349700b
Tam Love <dark_angel4eva@email.com>
2022-09-20 16:22:40 +0100

    Added post request for contact form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,39 +3,76 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { Waypoint } from 'react-waypoint'
 import { isEmpty } from '../utils/utils'
+import axios from 'axios'
 
-const sendForm = (e, formData, formErrors, setFormErrors) => {
+const sendForm = (e, formData, setFormData, setFormErrors, setFormStatus) => {
     e.preventDefault()
-    console.log(validateForm(formData, formErrors, setFormErrors))
+
+    if(validateForm(formData, setFormErrors)) {
+        axios
+            .post(`https://formspree.io/f/${process.env.GATSBY_FORMSPREE_ID}`, formData, {
+                headers: {
+                    Accept: "application/json",
+                }
+            })
+            .then(({data}) => {
+                console.log(data)
+                if(data.ok) {
+                    setFormStatus(formFeedback[6])
+                    setFormData({
+                        name: '',
+                        email: '',
+                        subject: 'NetMinded Contact Message',
+                        message: '',
+                        _gotcha: ''
+                    })
+                } else {
+                    if (Object.hasOwn(data, 'errors')) {
+                        setFormStatus(data["errors"].map(error => error["message"]).join(", "))
+                    } else {
+                        setFormStatus(formFeedback[5])
+                    }
+                }
+                setTimeout(() => setFormStatus('') , 8000)
+            })
+            .catch((error) => {
+                setFormStatus(formFeedback[5])
+                setTimeout(() => setFormStatus('') , 8000)
+            })
+    }
 }
 
-const validateForm = (formData, formErrors, setFormErrors) => {
+const validateForm = (formData, setFormErrors) => {
     let formPass = true
     let errorName = '', errorEmail = '', errorMsg = ''
 
-    if(isEmpty(formData.formName)) {
+    if(isEmpty(formData.name)) {
         formPass = false
         errorName = formFeedback[0]
     } else {
-        if(!charactersOnly(formData.formName)) {
+        if(!charactersOnly(formData.name)) {
             formPass = false
             errorName = formFeedback[1]
         }
     }
 
-    if(isEmpty(formData.formEmail)) {
+    if(isEmpty(formData.email)) {
         formPass = false
         errorEmail = formFeedback[2]
     } else {
-        if(!validEmailFormat(formData.formEmail)) {
+        if(!validEmailFormat(formData.email)) {
             formPass = false
             errorEmail = formFeedback[3]
         }
     }
 
-    if(isEmpty(formData.formMessage)) {
+    if(isEmpty(formData.message)) {
         formPass = false;
         errorMsg = formFeedback[4]
+    }
+
+    if(!isEmpty(formData._gotcha)) {
+        formPass = false;
     }
 
     setFormErrors({
@@ -44,7 +81,6 @@ const validateForm = (formData, formErrors, setFormErrors) => {
         messageError: errorMsg
     })
 
-    console.log(formErrors)
     return formPass
 }
 
@@ -62,54 +98,65 @@ const formFeedback = [
     'Please enter valid characters for your name.',
     'Please enter your email address.',
     'Please enter a valid email address.',
-    'Please enter a message.'
+    'Please enter a message.',
+    'Sorry, there was an issue sending your form request. Please try again later!',
+    'Thank you for your message, we\'ll be in touch soon!'
 ]
 
 const ContactPage = () => {
-    const [isHero, setIsHero] = React.useState(true)
+    const [isHero, setIsHero] = React.useState(false)
     const [formData, setFormData] = React.useState({
-        formName: '',
-        formEmail: '',
-        formSubject: 'NetMinded Contact Message',
-        formMessage: ''
+        name: '',
+        email: '',
+        subject: 'NetMinded Contact Message',
+        message: '',
+        _gotcha: ''
     })
     const [formErrors, setFormErrors] = React.useState({
         nameError: '',
         emailError: '',
         messageError: ''
     })
-
-    const url = `https://docs.google.com/forms/d/e/1FAIpQLSd0-WsHvaofWyFGZeQPVjIioNLygiyKAbmiwmXnLBGvMqn6Ng/viewform?usp=pp_url&entry.893828909=${formData.formName}&entry.2118704329=${formData.formEmail}&entry.1626097844=${formData.formSubject}&entry.1195998304=${formData.formMessage}`
+    const [formStatus, setFormStatus] = React.useState('')
     return (
         <Layout isHero={isHero}>
-            <header className="contact-page-header">
-                <h1>Contact Us</h1>
-                <h3>If you're interested in partnering with us get in touch!</h3>
-            </header>
-            <Waypoint onEnter={() => setIsHero(true)} onLeave={() => setIsHero(false)} topOffset={100} />
+            <Waypoint onEnter={() => setIsHero(true)} onLeave={() => setIsHero(false)} topOffset={100}>
+                <header className="contact-page-header">
+                    <h1>Contact Us</h1>
+                    <h3>If you're interested in partnering with us get in touch!</h3>
+                </header>
+            </Waypoint>
             <div className='contact-container'>
                 <div className='container'>
-                    <form className='row' novalidate>
+                    <form className='row'>
                         <div className="offset-3 col-6">
-                            <label for="validationCustom01" className="form-label">Name</label>
-                            <input type="text" class="form-control" id="validationCustom01" value={formData.formName} onChange={(e) => setFormData({...formData, formName: e.target.value})} required />
+                            <label for="formName" className="form-label">Name</label>
+                            <input type="text" name="name" class="form-control" id="formName" value={formData.name} onChange={(e) => {setFormData({...formData, name: e.target.value}); setFormErrors({...formErrors, nameError: ''})}} required />
                             {formErrors.nameError.length > 0 && <p>{formErrors.nameError}</p>}
                         </div>
                         <div className="offset-3 col-6">
-                            <label for="validationCustom02" className="form-label">Email Address</label>
-                            <input type="email" class="form-control" id="validationCustom02" value={formData.formEmail} onChange={(e) => setFormData({...formData, formEmail: e.target.value})} required />
+                            <label for="formEmail" className="form-label">Email Address</label>
+                            <input type="email" name="email" class="form-control" id="formEmail" value={formData.email} onChange={(e) => {setFormData({...formData, email: e.target.value}); setFormErrors({...formErrors, emailError: ''})}} required />
                             {formErrors.emailError.length > 0 && <p>{formErrors.emailError}</p>}
                         </div>
                         <div className="offset-3 col-6">
-                            <label for="validationCustom03" className="form-label">Message</label>
-                            <textarea class="form-control" id="validationCustom03" value={formData.formMessage} onChange={(e) => setFormData({...formData, formMessage: e.target.value})} required />
+                            <input type="text" name="subject" class="form-control" id="formSubject" value={formData.subject} required disabled/>
+                        </div>
+                        <div className="offset-3 col-6">
+                            <label for="formMessage" className="form-label">Message</label>
+                            <textarea name="message" class="form-control" id="formMessage" value={formData.message} onChange={(e) => {setFormData({...formData, message: e.target.value}); setFormErrors({...formErrors, messageError: ''})}} required />
                             {formErrors.messageError.length > 0 && <p>{formErrors.messageError}</p>}
                         </div>
                         <div className='offset-3 col-6'>
-                            <button className='btn' onClick={(e) => sendForm(e, formData, formErrors, setFormErrors)}>
-                            Send Message
-                            </button>
+                            <input type="text" name="_gotcha" class="form-control" id="formContact" value={formData._gotcha} onChange={(e) => setFormData({...formData, _gotcha: e.target.value})} />
                         </div>
+                        <div className='offset-3 col-6'>
+                            <button className='btn' formnovalidate onClick={(e) => sendForm(e, formData, setFormData, setFormErrors, setFormStatus)}>
+                                Send Message
+                            </button>
+                            {formStatus.length > 0 && <p>{formStatus}</p>}
+                        </div>
+                        
                     </form>
                 </div>
             </div>
